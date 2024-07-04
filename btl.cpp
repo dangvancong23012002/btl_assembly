7ac930402d7f099d1408be88737cf95feade30b9
dangvancong23012002 <20a10010377@students.hou.edu.vn>
2024-07-04 20:53:53 +0700

    code big exercise session 1

---
+++ btl.cpp
@@ -0,0 +1,51 @@
+#include <iostream.h>
+#include <conio.h>
+
+extern EXIT();
+void main(void) {
+	char tl;
+	int chon;
+	clrscr();
+	cout << "\n\tTRUONG DAI HOC MO HA NOI";
+	cout << "\n\tKHOA CONG NGHE THONG TIN";
+	cout << "\n\t----------***-----------";
+	cout << "\n\n\tBAI TAP LON MON LTHT";
+	cout << "\n\n\tSinh vien thuc hien:";
+	cout << "\n\n\tDang Van Cong - 2010A02";
+	cout << "\n\tLe Hai Doan   - 2010A02";
+	cout << "\n\tPham Van Long - 2110A05";
+	cout << "\n\t-------------------------";
+	cout << "\n\tCo tiep tuc chuong trinh khong(c/k)?";
+	tl = getch();
+	if (tl == 'c') {
+		PS:
+			clrscr();
+			cout << "\n\t               De 1";
+			cout << "\n\n\tCAC CHUC NANG TAO MD, TONG SL, CONG COM";
+			cout << "\n\t---------------------------------------";
+			cout << "\n\t1 ... Chuc nang MD";
+			cout << "\n\t2 ... Tong le day so nguyen";
+			cout << "\n\t3 ... Cong COM va dia chi";
+			cout << "\n\t4 ... Thoat ve DOS";
+			cout << "\n\t---------------------------------------";
+			cout << "\n\tLua chon cua ban: "; cin >> chon;
+			switch(chon) {
+				case 1:
+					cout << "\n\tTao MD";
+					getch();
+					break;
+				case 4:
+					EXIT();
+					break;
+				default:
+					cout << "\n\tChon sai!!!";
+					getch();
+					break;
+			}
+		cout << "\n\tCo thuc hien chuc nang khac(c/k)?";
+		tl = getch();
+		if (tl == 'c') {
+			goto PS;
+		}
+	}
+}
