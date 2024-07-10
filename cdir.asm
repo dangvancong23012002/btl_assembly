e3c4eb1c0d26aaaa3cba2b96813c520f541503f5
dangvancong23012002 <20a10010377@students.hou.edu.vn>
2024-07-10 20:08:38 +0700

    code create dir

--- cdir.asm
+++ cdir.asm
@@ -5,8 +5,13 @@ INCLUDE lib1.asm
 	dir1	db 13, 10, '	       CHUC NANG TAO THU MUC'
 			db 13, 10, '	------------------------------------'
 			db 13, 10, '	Hay vao ten thuc muc can tao: $'
+	err_dir db 13, 10, '	Khong tao duoc thu muc! $'
+	suc_dir db 13, 10, '	Tao thu muc thanh cong! $'
 	tieptuc db 13, 10, '	------------------------------------'
 			db 13, 10, '	Co tiep tuc chuong trinh khong(c/k)? $'
+	buff 	db 40
+			db ?
+	dir_name db 40 dup(?)
 .CODE
 	PUBLIC @CRE_DIR$qv
 @CRE_DIR$qv PROC
@@ -16,6 +21,17 @@ INCLUDE lib1.asm
 	L_CT0:
 		clrscr
 		HienString dir1
+		lea dx, buff ; ds:dx trỏ đến đầu buff 
+		call GET_DIR_NAME ; Gọi hàm nhận tên thư mục cần tạo 
+		lea dx, dir_name ; dx trỏ đến đầu xâu chứa tên thư mục 
+		mov ah, 39h ; Chức năng tạo lập thư mục 
+		int 21h
+		jnc L_CT1
+		HienString err_dir
+		jmp CONTINUE
+		
+	L_CT1:
+		HienString suc_dir
 		
 	CONTINUE:
 		HienString tieptuc
